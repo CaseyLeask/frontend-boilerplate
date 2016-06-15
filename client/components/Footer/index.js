e0e77c00f71a2f54b3e21e73ddbbd4f3825ac016
Casey Leask <casey.leask@rea-group.com>
2016-06-15 16:52:32 +1000

    Ripped out text

--- client/components/Footer/index.js
+++ client/components/Footer/index.js
@@ -3,43 +3,7 @@ import React, { Component } from 'react'
 import { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } from '../../constants/filters'
 import classnames from 'classnames'
 import style from './style.css'
-
-const translatedText = {
-  showAll: {
-    '中文': '所有',
-    'English': 'All'
-  },
-  showActive: {
-    '中文': '活性',
-    'English': 'Active'
-  },
-  showCompleted: {
-    '中文': '完成',
-    'English': 'Completed'
-  },
-  todoCount: {
-    '中文': function(count) {
-      switch(count) {
-        case 0:
-          return [<strong>0</strong>, '物品离开'];
-        case 1:
-          return ['还剩', <strong>1</strong>, '个项目'];
-        default:
-          return [<strong>{count}</strong>, '项左'];
-      }
-    },
-    'English': function(count) {
-      switch(count) {
-        case 0:
-          return [<strong>No</strong>, ' items left'];
-        case 1:
-          return [<strong>1</strong>, ' item left'];
-        default:
-          return [<strong>{count}</strong>, ' items left'];
-      }
-    }
-  }
-};
+import translatedText from './translatedText'
 
 const FILTER_TITLES = {
   [SHOW_ALL]: translatedText.showAll,
